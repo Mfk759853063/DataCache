f1adf7fd23c41a91dba8347c03399a170d91edc7
zhoucan <zhoucan123@vip.qq.com>
2016-01-08 15:54:56 +0800

    Initial Commit

---
+++ KNDataCacheSimple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNDataCacheSimple
+//
+//  Created by hzdlapple2 on 16/1/8.
+//  Copyright © 2016年 hzdlapple2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNDataCacheSimple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNDataCacheSimple
+//
+//  Created by hzdlapple2 on 16/1/8.
+//  Copyright © 2016年 hzdlapple2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
